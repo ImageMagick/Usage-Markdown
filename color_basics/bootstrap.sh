247bb2740b819b29b1494ca06382533192aabe6a
Mark Setchell <mark@thesetchells.com>
2016-02-11 17:13:43 +0000

    Added back in hsl_named_colours since making it OSX compatible

--- color_basics/bootstrap.sh
+++ color_basics/bootstrap.sh
@@ -7,9 +7,4 @@ ln -s ../images/shape_disks.gif  disks.gif
 
 convert rose: -colorspace CMY -separate separate_CMY_%d.gif
 
-which tac >/dev/null
-if [ $? -eq 0 ]
-then
-    ../scripts/hsl_named_colors named_colors.png
-fi
-
+../scripts/hsl_named_colors named_colors.png
